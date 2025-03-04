3e6667f202b86c98eaafb36cab71003c20d7fcd9
Ismael Heredia <ismaelheredia16@gmail.com>
2025-03-04 11:59:00 -0300

    Se mejoró el diseño de los mensajes toast

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,36 +1,10 @@
-
-import { mdiCheck, mdiAlert, mdiCancel } from '@mdi/js';
-
 export function generateToast(status: string) {
     const timeout = Number(import.meta.env.VITE_TOAST_TIMEOUT);
-    if (status == "success") {
-        return {
-            duration: timeout,
-            cardProps: {
-                color: 'success',
-                class: 'toast',
-            },
-            prependIcon: mdiCheck
-        }
-    }
-    else if (status == "warning") {
-        return {
-            duration: timeout,
-            cardProps: {
-                color: 'warning',
-                class: 'toast',
-            },
-            prependIcon: mdiAlert
-        }
-    }
-    else if (status == "error") {
-        return {
-            duration: timeout,
-            cardProps: {
-                color: 'success',
-                class: 'toast',
-            },
-            prependIcon: mdiCancel
-        }
-    }
+    return {
+        duration: timeout,
+        cardProps: {
+            color: status,
+            class: 'toast text-h2',
+        },
+    };
 }
